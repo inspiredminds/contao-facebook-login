0de9aa5339e372472228c64094166523153b0b20
David Molineus <david.molineus@netzmacht.de>
2020-02-21 10:18:03 +0100

    Use early exit

--- src/FacebookLoginBundle/Modules/AbstractFacebookModule.php
+++ src/FacebookLoginBundle/Modules/AbstractFacebookModule.php
@@ -25,42 +25,44 @@ abstract class AbstractFacebookModule extends Module
 {
     protected function handleLoginForm() : void
     {
+        if (Input::post('FORM_SUBMIT') !== 'tl_facebook_login_'.$this->id) {
+            return;
+        }
+
         $session = System::getContainer()->get('session');
 
-        if (Input::post('FORM_SUBMIT') !== 'tl_facebook_login_'.$this->id) {
-            // Get the login data to be retrieved
-            $this->fbLoginData = deserialize($this->fbLoginData, true);
+        // Get the login data to be retrieved
+        $this->fbLoginData = deserialize($this->fbLoginData, true);
 
-            // Prepare the facebook permissions
-            $permissions = ['public_profile'];
+        // Prepare the facebook permissions
+        $permissions = ['public_profile'];
 
-            // Add email permission
-            if (\in_array('email', $this->fbLoginData, true)) {
-                $permissions[] = 'email';
-            }
-
-            // Get the custom permissions
-            $permissions = \array_filter(\array_unique(\array_merge($permissions, StringUtil::splitCsv($this->fbLoginPerms))));
-
-            // Auto login
-            if (isset($_POST['autologin']) && $this->autologin) {
-                $session->set('facebook_login_autologin', (bool) $_POST['autologin']);
-            }
-
-            // get the Facebook SDK
-            $fb = FacebookFactory::create();
-
-            $helper = $fb->getRedirectLoginHelper();
-            $router = System::getContainer()->get('router');
-            $callbackUrl = $router->generate('facebook_login.controller.callback', [], UrlGeneratorInterface::ABSOLUTE_URL);
-            $loginUrl = $helper->getLoginUrl($callbackUrl, $permissions);
-
-            // set some session variables
-            global $objPage;
-            $session->set('facebook_login_module', $this->id);
-            $session->set('facebook_login_page', $objPage->id);
-
-            Controller::redirect($loginUrl);
+        // Add email permission
+        if (\in_array('email', $this->fbLoginData, true)) {
+            $permissions[] = 'email';
         }
+
+        // Get the custom permissions
+        $permissions = \array_filter(\array_unique(\array_merge($permissions, StringUtil::splitCsv($this->fbLoginPerms))));
+
+        // Auto login
+        if (isset($_POST['autologin']) && $this->autologin) {
+            $session->set('facebook_login_autologin', (bool) $_POST['autologin']);
+        }
+
+        // get the Facebook SDK
+        $fb = FacebookFactory::create();
+
+        $helper = $fb->getRedirectLoginHelper();
+        $router = System::getContainer()->get('router');
+        $callbackUrl = $router->generate('facebook_login.controller.callback', [], UrlGeneratorInterface::ABSOLUTE_URL);
+        $loginUrl = $helper->getLoginUrl($callbackUrl, $permissions);
+
+        // set some session variables
+        global $objPage;
+        $session->set('facebook_login_module', $this->id);
+        $session->set('facebook_login_page', $objPage->id);
+
+        Controller::redirect($loginUrl);
     }
 }
